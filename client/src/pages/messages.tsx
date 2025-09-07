1f6e04b2dd4e5980afa9773ac201e0bac10b02db
Faigy Eichenstein <feichenstein@megadatahs.com>
2025-09-07 10:01:23 -0400

    make height of page shorter

--- client/src/pages/messages.tsx
+++ client/src/pages/messages.tsx
@@ -101,7 +101,7 @@ export default function Messages() {
   return (
     <div
       className="flex flex-col w-full min-h-0 bg-gradient-to-b from-muted/30 to-muted/10"
-      style={{ minHeight: '100svh' }}
+      style={{ minHeight: '60svh', maxHeight: '90svh', height: '90svh' }}
     >
       {/* Message List (scrollable) */}
       <div className="flex-1 flex flex-col min-h-0">
